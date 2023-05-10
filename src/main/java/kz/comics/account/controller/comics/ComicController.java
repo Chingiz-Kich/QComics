94879c41e33ab09e644d5ff2580d87bb8bf7d166
Chingiz Kissikov <chingizik093@gmail.com>
2023-05-10 12:31:01 +0600

    delete controller fixed - transactional added

--- src/main/java/kz/comics/account/controller/comics/ComicController.java
+++ src/main/java/kz/comics/account/controller/comics/ComicController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * @author Chingiz
+ */
 @RestController
 @RequestMapping(path = "api/v1/comic")
 @RequiredArgsConstructor
